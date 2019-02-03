d27a3891f1db73787ae8b55ce1dbbb7dc6dde247
jacobbogers <jkfbogers@gmail.com>
2019-02-03 23:29:39 +0100

    create from scratch demo tutorials for material-ui

---
+++ notes.md
@@ -0,0 +1,19 @@
+//https://www.valentinog.com/blog/react-webpack-babel/
+
+npm install @material-ui/core
+
+npm install @material-ui/icons
+
+icon tool selector: https://material.io/tools/icons/
+
+more material icons: https://materialdesignicons.com/
+
+font awesome icons: https://fontawesome.com/icons?d=gallery
+
+The "Icon" component uses *(font ligature)*
+
+```javascript
+import Icon from '@material-ui/core/Icon';
+
+<Icon>star</Icon>
+```
---
+++ webpack.config.js
@@ -0,0 +1,71 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin")
+const MiniCssExtractPlugin = require("mini-css-extract-plugin")
+const CleanWebpackPlugin = require('clean-webpack-plugin')
+const { resolve } = require('path')
+
+module.exports = function () {
+
+    const id = Math.random()
+
+    const rc = {
+        mode: 'development',
+        entry: {
+            app: './src/App.js'
+        },
+        output: {
+           // path: resolve(__dirname, './dist'),
+            filename: `[name]/bundle-${id}.js`
+        },
+        module: {
+            rules: [{
+                    test: /\.(jsx?)$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: "babel-loader"
+                    }
+                },
+                {
+                    test: /\.(png|jpg|gif)$/,
+                    use: [{
+                        loader: 'file-loader',
+                        options: {},
+                    }, ],
+                },
+                {
+                    test: /\.scss$/,
+                    use: [{
+                            loader: MiniCssExtractPlugin.loader // creates style nodes from JS strings
+                        },
+                        {
+                            loader: "css-loader", // translates CSS into CommonJS
+                            options: {
+                                sourceMap: true
+                            }
+                        },
+                        {
+                            loader: "sass-loader", // compiles Sass to CSS
+                            options: {
+                                sourceMap: true
+                            }
+                        }
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebPackPlugin({
+                title:'📖 👨‍🎓 tutorial'
+            }),
+            new CleanWebpackPlugin(['dist'],
+            { 
+              verbose: true,
+              dry: false
+            }),
+            new MiniCssExtractPlugin({
+                filename: "[name].css",
+                chunkFilename: "[id].css"
+            })
+        ]
+    }
+    return rc
+}
