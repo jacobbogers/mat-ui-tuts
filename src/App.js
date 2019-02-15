8bfb93979578ea6ff362101383d630b0be06852b
jacobbogers <jkfbogers@gmail.com>
2019-02-16 00:26:19 +0100

    deleted 3 trial branches

--- src/App.js
+++ src/App.js
@@ -11,51 +11,11 @@ const theme = createMuiTheme({
   }
 });
 
-// demos advanced
 
-import UseThemeHOC from './css-in-js/advanced-theme-hoc';
-import UseThemeHook from './css-in-js/advanced-theme-hook';
-import UseThemeNesting from './css-in-js/advanced-theme-nesting';
-
-// system (alpha)
-import Demo from './system-alpha/basics/demo';
-import Box from './Box';
-
-import Variant from './system-alpha/basics/variants';
-import CssProp from './system-alpha/basics/css-props'
-// app.scss
-import './app.scss';
-
-const styles = {
-	display: 'flex',
-	flexDirection: 'column',
-	alignItems: 'center',
-	justifyContent: 'center',
-	maxWidth: '600px'
-};
 
 function App() {
 	return (
-		<div id="container" style={styles}>
-			<CssProp />
-			<Variant />
-			<Box p={{ xs: 2, sm: 3, md: 4 }} color="white" bgcolor="palevioletred">
-				Styled components
-			</Box>
-			<Box color="white" bgcolor="palevioletred" p={1}>
-				JSS
-			</Box>
-			<Box p={1}>4px</Box>
-			<Box p={2}>8px</Box>
-			<Box p={-1}>-4px</Box>
-			<Demo />
-			<AdaptingHOC />
-			<AdaptingHook />
-			<AdaptingStyledComponents />
-			<UseThemeHOC />
-			<UseThemeHook />
-			<UseThemeNesting />
-		</div>
+		<div> hello world </div>
 	);
 }
 
