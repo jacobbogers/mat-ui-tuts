9e91cdfcceabddb3e223a10ae4b784d70f4125c9
jacobbogers <jkfbogers@gmail.com>
2019-02-18 00:53:41 +0100

    iconbuttons added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import ButtonAppBar from './demos/ButtonAppBar';
 import PrimaryFieldAppBar from './demos/PrimaryFieldAppBar';
 import Buttons01 from './demos/Buttons01';
 import Fabs from './demos/Fabs';
+import IconButtons from './demos/IconButtons';
+
 import './app.scss';
 //import 'material-icons';
 
@@ -15,6 +17,7 @@ ReactDOM.render(
 		<SimpleAppBar />
 		<Buttons01 />
 		<Fabs />
+		<IconButtons/>
 	</div>,
 	document.querySelector('#app')
 );
