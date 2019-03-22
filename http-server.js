01c6ffcc362968f0b463fce53f0ab3c75f7399d0
jacobbogers <jkfbogers@gmail.com>
2019-03-22 23:48:51 +0100

    next doenst exist on request event of http.server

---
+++ http-server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+
+function bleedIncommingMessage (incMsg, encoding) {
+	return new Promise((resolve, reject) => {
+		const data = [];
+		incMsg.setEncoding(encoding || 'utf8');
+		incMsg.on('data', chunk => {
+			data.push(chunk);
+		});
+		incMsg.on('end', () => resolve(data.join('')));
+		incMsg.on('error', err => {
+			reject(String(err));
+		});
+	});
+}
+
+const server = http.createServer( async (incMsg, servResp, next) => {
+	const url = incMsg.url;
+	const rawHeaders = incMsg.rawHeaders;
+	const method = incMsg.method;
+
+	console.log({ method, url, rawHeaders });
+    const message = await bleedIncommingMessage(incMsg, 'utf8');
+	servResp.writeHead(200, {
+		'Content-Length': 0,
+		'Content-Type': 'text/plain'
+	});
+    servResp.end();
+    console.log('message received [', message,']')
+    console.log('next:',!!next, String(next));
+});
+
+const port = 8082
+server.listen(port, ()=>{
+    console.log(`listening on: ${port}`);
+});
